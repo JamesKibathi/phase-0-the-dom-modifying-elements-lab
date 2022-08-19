5b7cd9dd6d56ec9e29470ac58bae8cd286cac5b2
James Njenga <knjenga94@gmail.com>
2022-08-20 02:50:59 +0300

    attempted the challenge

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+
+const main=document.querySelector('#main');
+main.remove();
+
+for(let i=0;i<3;i++){
+const newHeader=document.createElement("h1");
+newHeader.id="victory";
+document.body.append(newHeader);
+}
+
+document.querySelectorAll("h1")[2].innerHTML = "YOUR-NAME is the champion";
+
+
+
+
+
+    
